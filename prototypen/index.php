1dd6c35583aa5919ded07463e1f3124ca4ce2cdb
timohinze <mail@timohinze.com>
2021-10-29 11:24:55 +0200

    Update index.php

--- prototypen/index.php
+++ prototypen/index.php
@@ -1,5 +1,22 @@
 <h1>Prototypen</h1>
 
-<p>Startseite: <br>
-https://supercomputerstudio.com/clients/hkw-prototypen/prototypen/startseite/
-</p>
+<h2>Startseite</h2>
+<p><a href="https://supercomputerstudio.com/clients/hkw-prototypen/prototypen/startseite/">Startseite</a></p>
+
+<h2>Kalender</h2>
+<p><a href="https://supercomputerstudio.com/clients/hkw-prototypen/prototypen/kalender/">Kalender</a></p>
+
+
+<h2>Projektstartseiten</h2>
+<p><a href="https://supercomputerstudio.com/clients/hkw-prototypen/prototypen/projektstartseiten/bauhaus-imaginista.php">bauhaus imaginista</a></p>
+<p><a href="https://supercomputerstudio.com/clients/hkw-prototypen/prototypen/projektstartseiten/kuratorisches-statement.php">Kuratorisches Statement</a></p>
+<p><a href="https://supercomputerstudio.com/clients/hkw-prototypen/prototypen/projektstartseiten/lorem-ipsum.php">Lorem Ipsum</a></p>
+<p><a href="
+https://supercomputerstudio.com/clients/hkw-prototypen/prototypen/projektstartseiten/internationaler-literaturpreis.php">Internationaler Literaturpreis</a></p>
+<p><a href="
+https://supercomputerstudio.com/clients/hkw-prototypen/prototypen/projektstartseiten/saison-der-wirbelsturme.php">Saison der Wirbelstürme</a></p>
+
+
+
+
+
