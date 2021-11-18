a7131d20be69e3bbc40cd0086d34539b2b161383
timo <mail@timohinze.com>
2021-11-18 11:51:04 +0100

    projektübersichtsseite weiter

---
+++ assets/js/projektuebersicht.js
@@ -0,0 +1,18 @@
+$( "#show-filter-years" ).click(function() {
+  $( ".project-filter-years" ).toggle();
+});
+$( "#show-filter" ).click(function() {
+  $( ".project-filter" ).toggle();
+});
+
+
+ $( "#filter__year--all" ).click(function() {
+   if ($(this).prop('checked')) {
+     $( '.project-years input[type="checkbox"]' ).prop( "checked", true );
+     $(this).next("label").text("Zurücksetzen");
+   }else{
+     $( '.project-years input[type="checkbox"]' ).prop( "checked", false );
+     $(this).next("label").text("Alle Jahre");
+
+   }
+ });
