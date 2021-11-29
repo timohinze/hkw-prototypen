9424c74a484aad17b1a3d7318db279d63bf6226e
timo <mail@timohinze.com>
2021-11-29 13:45:22 +0100

    css details

--- assets/js/multiteaser.js
+++ assets/js/multiteaser.js
@@ -2,13 +2,13 @@
 $( document ).ready(function() {
 
   $(".multiteaser-item--accordion").addClass('multiteaser-item--accordion-js');
-  $(".multiteaser-item--accordion .multiteaser__text").hide();
-  $(".multiteaser-item--accordion .multiteaser__content").css('cursor', 'pointer');
+  $(".multiteaser-item--accordion .multiteaser-item__text").hide();
+  $(".multiteaser-item--accordion .multiteaser-item__content").css('cursor', 'pointer');
   $(".multiteaser-item--accordion .multiteaser__toggle-btn").show();
 
 });
 
-$( ".multiteaser-item--accordion .multiteaser__content" ).click(function() {
+$( ".multiteaser-item--accordion .multiteaser-item__content" ).click(function() {
   $(this).parents(".multiteaser-item--accordion").toggleClass('multiteaser-item--accordion-open');
 
 })
--- functions/footer.php
+++ functions/footer.php
@@ -5,8 +5,8 @@
 <footer class="page-footer">
   <div class="page-footer-gradient">
   <nav class="page-footer-menu">
-    <div class="page-footer-menu-column">
-      <div class="page-footer-menu-column__header"><a href="#">Programm</a></div>
+    <div class="page-footer-menu__column">
+      <div class="page-footer-menu__header"><a href="#">Programm</a></div>
         <ul class="page-footer-submenu">
           <li><a href="#">Heute</a></li>
           <li><a href="#">2019</a></li>
@@ -14,8 +14,8 @@
           <li><a href="#">Das Anthropozän am HKW</a></li>
         </ul>
     </div>
-    <div class="page-footer-menu-column">
-      <div class="page-footer-menu-column__header"><a href="#">Das Haus</a></div>
+    <div class="page-footer-menu__column">
+      <div class="page-footer-menu__header"><a href="#">Das Haus</a></div>
       <ul class="page-footer-submenu">
         <li><a href="#">Über uns</a></li>
         <li><a href="#">Architektur</a></li>
@@ -26,8 +26,8 @@
       </ul>
 
     </div>
-    <div class="page-footer-menu-column">
-      <div class="page-footer-menu-column__header"><a href="#">Service</a></div>
+    <div class="page-footer-menu__column">
+      <div class="page-footer-menu__header"><a href="#">Service</a></div>
       <ul class="page-footer-submenu">
         <li><a href="#">Tickets</a></li>
         <li><a href="#">Öffnungszeiten & Anfahrt </a></li>
@@ -35,8 +35,8 @@
         <li><a href="#">Shop</a></li>
       </ul>
     </div>
-    <div class="page-footer-menu-column">
-      <div class="page-footer-menu-column__header"><a href="#">Kontakt</a></div>
+    <div class="page-footer-menu__column">
+      <div class="page-footer-menu__header"><a href="#">Kontakt</a></div>
       <ul class="page-footer-submenu">
         <li><a href="#">Presse</a></li>
         <li><a href="#">Datenschutzerklärung</a></li>
@@ -50,7 +50,7 @@
 <adress class="page-footer-adress">
   Haus der Kulturen der Welt<br>
   John-Foster-Dulles-Allee 10, 10557 Berlin<br>
-  Tel + 49 30 397 87 0<br>
+  <a href="tel:+4930397870"> Tel + 49 30 397 87 0</a><br>
   <a href="mailto:info@hkw.de"  title="info@hkw.de">info@hkw.de</a>
 </adress>
 
@@ -67,10 +67,12 @@
 
 <div class="page-footer-media">
  <img src="https://www.hkw.de/media/bilder/2020/cc__world/meg_stuart_intermission_imgsize_buehne.jpg" alt="lorem ipsum"/>
-<div class="page-footer__logo">
+<div class="page-footer-logo">
 <a href="#">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130 56">
-         <path d="M29.3 18.6H7.4V.1H-.2v44.4h7.6V26h21.9v18.5h7.6V.1h-7.6v18.5zm19-2.3V.1h-7.8v44.4h7.8V28.2L78 44.5v-8.7L52.4 22.3 78 8.8V.1L48.3 16.3zM109.5.1h-8.6L89.3 29.8V.1h-7.7v44.4h8.9l11.4-26.9v26.9h8.9L129.9.1h-8.6l-11.7 29.7-.1-29.7zM-.2 55.8h.9v-3h3.5v3h.9v-6.6h-.8v2.9H.7v-2.9h-.9v6.6zm6.9-1.2c0 .8.6 1.3 1.6 1.3.7 0 1.2-.2 1.5-.7v.6h.8v-3.1c0-1.1-.6-1.7-1.8-1.7-1.2 0-1.8.7-1.9 1.6h.8c0-.5.4-.9 1.1-.9.7 0 1 .4 1 1 0 .2-.1.3-.6.4l-.9.2c-1.2.1-1.6.6-1.6 1.3zm3.1-.7c0 .9-.6 1.4-1.3 1.4-.6 0-.9-.3-.9-.7 0-.4.2-.7.8-.8l.9-.2c.2 0 .4-.1.5-.2v.5zm2.4.1c0 1.5.8 2 1.7 2 .7 0 1.2-.3 1.4-.8v.7h.8v-4.7h-.8v2.4c0 1-.5 1.7-1.2 1.7S13 55 13 54v-2.7h-.8V54zm5.4.5c0 1 .8 1.5 1.9 1.5 1.2 0 2-.5 2-1.5 0-.8-.6-1.1-1.3-1.2l-.9-.3c-.6-.1-.8-.3-.8-.6 0-.5.5-.7 1-.7.6 0 1 .3 1 .8h.8c0-1-.8-1.4-1.8-1.4s-1.9.5-1.9 1.5.7 1.2 1.4 1.3l.9.2c.5.1.7.2.7.6 0 .5-.4.8-1.2.8-.5 0-1.1-.2-1.1-.8l-.7-.2zm7.4-1c0 1.5.9 2.4 2.1 2.4.7 0 1.3-.2 1.6-.8v.7h.8v-7h-.8v3.1c-.3-.6-.8-.8-1.5-.8C26 51 25 52 25 53.5zm.9 0c0-1.1.6-1.8 1.4-1.8s1.4.6 1.4 1.7-.6 1.7-1.4 1.7c-.8.1-1.4-.5-1.4-1.6zm5 0c0 1.5.8 2.5 2.3 2.5 1.1 0 1.9-.7 2-1.5h-.8c-.1.4-.5.8-1.2.8-.8 0-1.3-.6-1.4-1.6h3.4v-.3c0-1.4-.8-2.2-2.1-2.2-1.2-.2-2.2.8-2.2 2.3zm2.2-1.7c.7 0 1.1.4 1.2 1.2h-2.5c.2-.8.7-1.2 1.3-1.2zm3.5 4h.8v-2.5c0-.8.3-1.5 1-1.5.2 0 .4 0 .6.2l.3-.8c-.2-.2-.5-.2-.7-.2-.6 0-1 .3-1.2.8v-.7h-.8v4.7zm6.2 0h.9v-2l1-1.1 2.4 3.1h1l-2.8-3.7 2.7-2.9h-1l-3.3 3.6v-3.6h-.9v6.6zm6.3-1.8c0 1.5.8 2 1.7 2 .7 0 1.2-.3 1.4-.8v.7h.8v-4.7h-.8v2.4c0 1-.5 1.7-1.2 1.7s-1.1-.3-1.1-1.3v-2.7h-.8V54zm5.7 1.8h.8v-7h-.8v7zm2.9-1.3c0 .9.5 1.4 1.4 1.4h.7v-.7h-.6c-.5 0-.7-.2-.7-.7v-2.6h1.2v-.7h-1.2v-1.4h-.8v1.4h-.9v.7h.9v2.6zM61 54c0 1.5.8 2 1.7 2 .7 0 1.2-.3 1.4-.8v.7h.9v-4.7h-.8v2.4c0 1-.5 1.7-1.2 1.7s-1.1-.3-1.1-1.3v-2.7H61V54zm5.7 1.8h.8v-2.5c0-.8.3-1.5 1-1.5.2 0 .4 0 .6.2l.3-.8c-.2-.2-.5-.2-.7-.2-.6 0-1 .3-1.2.8v-.7h-.8v4.7zm3.2-2.3c0 1.5.8 2.5 2.3 2.5 1.1 0 1.9-.7 2-1.5h-.8c-.1.4-.5.8-1.2.8-.8 0-1.3-.6-1.4-1.6h3.4v-.3c0-1.4-.8-2.2-2.1-2.2-1.2-.2-2.2.8-2.2 2.3zm2.3-1.7c.7 0 1.1.4 1.2 1.2h-2.5c.1-.8.6-1.2 1.3-1.2zm3.4 4h.8v-2.4c0-1 .6-1.6 1.3-1.6.7 0 1.1.3 1.1 1.3v2.7h.8V53c0-1.5-.8-2-1.7-2-.7 0-1.2.3-1.5.8v-.7h-.8v4.7zm7.8-2.3c0 1.5.9 2.4 2.1 2.4.7 0 1.3-.2 1.6-.8v.7h.8v-7H87v3.1c-.3-.6-.8-.8-1.5-.8-1.2-.1-2.1.9-2.1 2.4zm.8 0c0-1.1.6-1.8 1.4-1.8.8 0 1.4.6 1.4 1.7s-.6 1.7-1.4 1.7c-.7.1-1.4-.5-1.4-1.6zm5.1 0c0 1.5.8 2.5 2.3 2.5 1.1 0 1.9-.7 2-1.5h-.8c-.1.4-.5.8-1.2.8-.8 0-1.3-.6-1.4-1.6h3.4v-.3c0-1.4-.8-2.2-2.1-2.2-1.3-.2-2.2.8-2.2 2.3zm2.2-1.7c.7 0 1.1.4 1.2 1.2h-2.5c.1-.8.7-1.2 1.3-1.2zm3.4 4h.8v-2.5c0-.8.3-1.5 1-1.5.2 0 .4 0 .6.2l.3-.8c-.2-.2-.5-.2-.7-.2-.6 0-1 .3-1.2.8v-.7h-.8v4.7zm7.3 0h1.1l1.5-5.6 1.5 5.6h1.1l1.7-6.6h-.9l-1.4 5.7-1.5-5.7h-1.1l-1.5 5.7-1.4-5.7h-.9l1.8 6.6zm7.4-2.3c0 1.5.8 2.5 2.3 2.5 1.1 0 1.9-.7 2-1.5h-.9c-.1.4-.5.8-1.2.8-.8 0-1.3-.6-1.4-1.6h3.4v-.3c0-1.4-.8-2.2-2.1-2.2-1.1-.2-2.1.8-2.1 2.3zm2.2-1.7c.7 0 1.1.4 1.2 1.2h-2.5c.2-.8.7-1.2 1.3-1.2zm3.5 4h.8v-7h-.8v7zm2.8-1.3c0 .9.5 1.4 1.4 1.4h.7v-.7h-.6c-.5 0-.7-.2-.7-.7v-2.6h1.2v-.7H119v-1.4h-.8v1.4h-.9v.7h.9v2.6h-.1z"></path>
+    <path d="M29.3,18.6H7.4V0.1h-7.6v44.4h7.6V26h21.9v18.5h7.6V0.1h-7.6C29.3,0.1,29.3,18.6,29.3,18.6z M48.3,16.3V0.1h-7.8v44.4h7.8
+    	V28.2L78,44.5v-8.7L52.4,22.3L78,8.8V0.1C78,0.1,48.3,16.3,48.3,16.3z M109.5,0.1h-8.6L89.3,29.8V0.1h-7.7v44.4h8.9l11.4-26.9v26.9
+    	h8.9l19.1-44.4h-8.6l-11.7,29.7C109.6,29.8,109.5,0.1,109.5,0.1z"/>
        </svg>
 </a>
      </div>
