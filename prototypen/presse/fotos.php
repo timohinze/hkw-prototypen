839e9fd7f8a64063789559cf566263d4dde6c241
timo <mail@timohinze.com>
2022-01-21 11:26:38 +0100

    … presse fotos

--- prototypen/presse/fotos.php
+++ prototypen/presse/fotos.php
@@ -9,11 +9,30 @@
   <div class="element title-element"><h1 class="">Pressefotos: Übersicht</h1>
   </div>
 
+  <div class="element text-element  element--padding">
 
-  <div class="press-photos-item">
+
+
+
+                  <div class="element-cols-1">
+                      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                      </p>
+                      <p>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                             </div>
+
+
+          </div>
+
+
+
+  <div class="element element--fullbleed press-photos-element ">
+
     <div class="press-photos-item__title">The Children Have to Hear Another Story – Alanis Obomsawin
       23.1. - 18.4.2022
     </div>
+
+
     <div class="press-photos-item__images">
 
       <div class="press-photos-item__image">
@@ -40,19 +59,26 @@
 
       <div class="press-photos-item__image">
         <figure>
-          <img src="https://www.hkw.de/media/pressebilder/projekte_2/2022_1/pressefotos_obomsawin/1a_AO__Alanis.SleepRock_colour_John_Kenney_FREI_imgsize_S.jpg">
+          <img src="https://www.hkw.de/media/pressebilder/projekte_2/2022_1/pressefotos_obomsawin/4_Mother_of_Many_Children_00_24_04_00_FREI_imgsize_S.jpg">
           <figcaption>Alanis Obomsawin | Sleeping During the Oka Crisis, 1990 | Foto: John Kenney</figcaption>
         </figure>
         <div class="press-photos-item__download">
           <a href="#" class="download-link">19187 kb, jpg Download</a>
         </div>
       </div>
+      <div class="press-photos-item__image">
+        <figure class="portrait-wrap"><img src="https://picsum.photos/500/600" alt="Lorem" class="portrait">
+        <figcaption>Lorem Ipsum</figcaption></figure>
 
+          <div class="press-photos-item__download">
+            <a href="#" class="download-link">19187 kb, jpg Download</a>
+        </div>
+      </div>
 
 
             <div class="press-photos-item__image">
               <figure>
-                <img src="https://www.hkw.de/media/pressebilder/projekte_2/2022_1/pressefotos_obomsawin/1a_AO__Alanis.SleepRock_colour_John_Kenney_FREI_imgsize_S.jpg">
+                <img src="https://www.hkw.de/media/pressebilder/projekte_2/2022_1/pressefotos_obomsawin/3_AO_When_all_the_leaves_are_gone_Filmstill_NFB_FREI_imgsize_S.jpg">
                 <figcaption>Alanis Obomsawin | Sleeping During the Oka Crisis, 1990 | Foto: John Kenney</figcaption>
               </figure>
               <div class="press-photos-item__download">
@@ -60,6 +86,17 @@
               </div>
             </div>
 
+
+
+            <div class="press-photos-item__image">
+              <figure>
+                <img src="https://www.hkw.de/media/pressebilder/projekte_2/2022_1/pressefotos_obomsawin/2_AO_Mariposa_Festival_Alanis_Obomsawin__Toronto_Telegram_fonds_F0433__ASC05824_FREI_imgsize_S.jpg">
+                <figcaption>Alanis Obomsawin | Sleeping During the Oka Crisis, 1990 | Foto: John Kenney</figcaption>
+              </figure>
+              <div class="press-photos-item__download">
+                <a href="#" class="download-link">19187 kb, jpg Download</a>
+              </div>
+            </div>
 
 
 
