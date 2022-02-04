5419fe59e4261ae0ff5e3af9f45cd22c906cf026
timo <mail@timohinze.com>
2022-02-04 13:44:23 +0100

    ..slideshow

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -7,8 +7,13 @@ $( document ).ready(function() {
     pageDots: false,
     prevNextButtons: true,
     imagesLoaded: true,
-   arrowShape: 'M49.2.4 26.8 50.3l22.6 49.4h22.2L48.9 50.2 71.3.4z',
-
+   // arrowShape: 'M49.2.4 26.8 50.3l22.6 49.4h22.2L48.9 50.2 71.3.4z',
+   arrowShape: {
+     x0: 10,
+     x1: 60, y1: 50,
+     x2: 60, y2: 30,
+     x3: 30
+   }
     // arrowShape: 'M17 0l17 17.1h-6.7L17 6.6 6.7 17.1H0L17 0z'
 
     // adaptiveHeight: true
