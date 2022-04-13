ce783dac1928fff4650abf08ee807097c36488ec
timo <mail@timohinze.com>
2022-04-13 20:29:54 +0200

    pfeile...

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,9 +1,10 @@
 
+
 // Medien Slideshow
 
 var pathDirections = {
-  view: 'M3.8,17.1H0V0h17.1v3.8H3.8V17.1z M24,6.8h-3.8v13.3H6.9v3.8H24V6.8z',
-  exit: 'M3.8,17.1H0V0h17.1v3.8H3.8V17.1z M24,6.8h-3.8v13.3H6.9v3.8H24V6.8z',
+  view: 'm6.8 3.7v-3.8h17.2v17.1h-3.8v-13.2zm10.3 20.3v-3.8h-13.3v-13.3h-3.7v17.1z',
+  exit: 'm23.2 3.6-2.8-2.8-8.4 8.4-8.4-8.4-2.8 2.8 8.4 8.4-8.4 8.4 2.8 2.8 8.4-8.4 8.4 8.4 2.8-2.8-8.4-8.4z',
 };
 
 var svgURI = 'http://www.w3.org/2000/svg';
@@ -21,6 +22,11 @@ Flickity.FullscreenButton.prototype.createIcon = function() {
   this.element.appendChild( svg );
 };
 
+
+
+
+
+
 $( document ).ready(function() {
   var $slideshows = $('.medien-slideshow-element .slideshow-element__slideshow').flickity({
     cellAlign: 'left',
@@ -29,12 +35,8 @@ $( document ).ready(function() {
     pageDots: false,
     prevNextButtons: true,
     imagesLoaded: true,
-   arrowShape: {
-     x0: 10,
-     x1: 60, y1: 50,
-     x2: 60, y2: 30,
-     x3: 30
-   },
+    arrowShape: 'm26.4 50 47.1 46.8v-23.9l-23.5-22.9 23.6-23.2v-23.6z',
+
      fullscreen: true,
 
 
@@ -49,8 +51,24 @@ $( document ).ready(function() {
 
 });
 
+// Header Bühnen Slideshows
+
+$( document ).ready(function() {
+  var $slideshows = $('.header-slideshow .slideshow-element__slideshow').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: false,
+    pageDots: true,
+    prevNextButtons: false,
+    imagesLoaded: true,
+     fullscreen: false,
+     arrowShape: 'm55.7 100-13.4-50 13.4-50 1.9.5-13.2 49.5 13.2 49.5z',
 
 
+  });
+
+});
 
 
 
@@ -91,7 +109,7 @@ $( document ).ready(function() {
 
 
 
-$('.slideshow-element__slideshow').each(function(index, element) {
+$('.medien-slideshow-element .slideshow-element__slideshow').each(function(index, element) {
 
     $(this).on('staticClick.flickity', function (event, pointer, cellElement, cellIndex) {
       // dismiss if cell was not clicked
